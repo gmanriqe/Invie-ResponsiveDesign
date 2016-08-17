c319b6bfc36719404a4411b37432594d41c686bd
Jesus Gonzales <gmanriqe@gmail.com>
2016-08-17 13:04:20 -0500

    Media Queries
    — Utilizando media queries con JS, para dispositivos con máximo ancho
    de 500px

--- js/js.js
+++ js/js.js
@@ -1,10 +1,22 @@
+var consulta = window.matchMedia('(max-width:500px)');
+consulta.addListener(mediaQuery); //se ejecutará en el momento que haga resize a la pantalla
 
 var $burguerButton = document.getElementById('burguer-button');
 var $menu = document.getElementById('menu');
 
-// addEventListener('que queremos escuchar','que queremos hacer');
-$burguerButton.addEventListener('touchstart',toggleMenu);
-
 function toggleMenu(){
 	$menu.classList.toggle('active');
 };
+
+function mediaQuery() {
+	if(consulta.matches) {
+		console.log('se cumplio la condicion');
+		$burguerButton.addEventListener('touchstart',toggleMenu);
+
+	}else {
+		console.log('NO! se cumplio la condicion');
+		$burguerButton.removeEventListener('touchstart',toggleMenu);
+
+	}
+}
+mediaQuery();
